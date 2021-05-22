610c8c46d53731fcc06dfc8414f5111465059641
A284Philipi <78768392+A284Philipi@users.noreply.github.com>
2021-05-22 14:24:25 -0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    long long int n;
+    cin >> n;
+    n = ((n + 1) * (n + 2)) / 2;
+    cout << n <<endl;
+    return 0;
+}
